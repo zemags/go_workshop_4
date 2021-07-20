643d7c9df9b25389ecf1c9e876c0b7f5352221d1
zemags <azmt2008@gmail.com>
2021-07-20 14:14:53 +0500

    #2 Add 404

--- main.go
+++ main.go
@@ -8,13 +8,28 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	// ResponseWriter for combainig http answer and return to user
 	// Request pointer for structure thats contain info about current request
-	w.Write([]byte("hello from 2 chapter"))
+	if r.URL.Path != "/" {
+		// return 404 if doesnt match to / or other contollers
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from 2 chapter"))
+}
+
+func showMemo(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Show Memo"))
+}
+
+func createMemo(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create Memo"))
 }
 
 func main() {
-	// create new router mux and register controler home
+	// create new router mux and register our controlers
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/memo", showMemo)
+	mux.HandleFunc("/memo/create", createMemo)
 
 	log.Println("start listening server")
 	if err := http.ListenAndServe(":8080", mux); err != nil {

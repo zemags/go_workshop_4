7304fc25b97a1e25054f5e03d5039589fc774323
zemags <azmt2008@gmail.com>
2021-07-20 15:24:50 +0500

    #3 Add HTTP headers

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"net/http"
 )
 
+const (
+	MethodNotAllowed = 405
+)
+
 func home(w http.ResponseWriter, r *http.Request) {
 	// ResponseWriter for combainig http answer and return to user
 	// Request pointer for structure thats contain info about current request
 	if r.URL.Path != "/" {
 		// return 404 if doesnt match to / or other contollers
 		http.NotFound(w, r)
+		// return to exit from func and do not continue next line
 		return
 	}
 	w.Write([]byte("Hello from 2 chapter"))
@@ -21,6 +26,15 @@ func showMemo(w http.ResponseWriter, r *http.Request) {
 }
 
 func createMemo(w http.ResponseWriter, r *http.Request) {
+	// POST request
+	if r.Method != http.MethodPost {
+		// add to header 'Allow: POST' user will know
+		w.Header().Set("Allow", http.MethodPost)
+
+		http.Error(w, "Method GET forbidden", MethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create Memo"))
 }
 
